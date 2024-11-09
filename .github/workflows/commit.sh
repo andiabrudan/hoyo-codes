a414de21aa7568c19c6a6eb2d8b5f4a3b1fc8394
Andrei Abrudan <andiabrudan@yahoo.com>
2024-11-09 23:23:28 +0000

    Added job step to commit changes
    Fixed commit step
    
    Update data from web scrape on 2024-11-10

--- .github/workflows/commit.sh
+++ .github/workflows/commit.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [[ `git status --porcelain` ]]; then
-    git config user.name "github-actions[bot]"
-    git config user.email "github-actions[bot]@users.noreply.github.com"
+if [[ `git status --porcelain codes-genshin.txt codes-hsr.txt codes-zzz.txt` ]]; then
+    git status
+    git config --local user.name "github-actions[bot]"
+    git config --local user.email "github-actions[bot]@users.noreply.github.com"
     git add codes-genshin.txt codes-hsr.txt codes-zzz.txt
     git commit -m "Update data from web scrape on $(date +'%Y-%m-%d')"
-    git push "https://${GITHUB_TOKEN}@github.com/${{ github.repository }}.git" HEAD:${{ github.ref }}
+    git push "https://${ACCESS_TOKEN}@github.com/${REPO}.git"
 else
     echo "No changes detected, skipping commit."
 fi
